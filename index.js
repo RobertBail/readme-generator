fbc78c658f77cf2c66c6d7bfd804baece04340b3
Robert Bailey <r.bail@hotmail.com>
2023-11-13 19:52:35 +1030

    readme generator updates

--- index.js
+++ index.js
@@ -56,7 +56,10 @@ inquirer
       type: "list",
       name: "License",
       message: "Select a license.",
-      choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3"],
+      choices: ["MIT [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)", 
+      "APACHE 2.0 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
+      "GPL 3.0 [![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)", 
+      "BSD 3 [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"],
     },
     {
       type: "input",
---
+++ sample readme/READMEsample.md
@@ -0,0 +1,26 @@
+# Good App
+  ## Description
+  To help all our customers
+  ## Table of Contents:
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  ## Installation
+  Use this command, npm install
+  ## Usage
+  To be used by everyone in the team
+  ## License
+  BSD 3 [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+  ## Contributing
+  By everyone who helps out in team
+  ## Tests
+  Use this command, npm test
+  ## Questions
+  Where to find us on GitHub? 
+  https://github.com/RobertBail
+  How to contact us?
+  r.bail@hotmail.com
+  
